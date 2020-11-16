ab7cd1404f0bef259e96311dff714bc3d349577b
milton-reyes <altf4616@outlook.com>
2020-11-16 16:30:41 -0500

    Pod was failing in kube cluster these changes prevent it from crashing. Added kubernetes deployment yaml for the question service.

--- src/main/java/com/revature/messaging/MessageService.java
+++ src/main/java/com/revature/messaging/MessageService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 import com.revature.services.QuestionService;
 
-@Service
+//@Service
 public class MessageService {
 	
 	private static Set<Integer> eventCache = new HashSet<>();

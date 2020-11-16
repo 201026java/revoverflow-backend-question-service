a61f4b27939d19b7321102ea144ff2188c82b49f
Mark Alsip <mark.alsip@comcast.net>
2020-11-16 09:21:20 -0700

    how is this even a thing?

--- src/main/java/com/revature/controller/QuestionController.java
+++ src/main/java/com/revature/controller/QuestionController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.revature.messaging.MessageService;
 import com.revature.messaging.Operation;
 
 import com.revature.models.Question;
+import com.revature.models.User;
 import com.revature.services.QuestionService;
 
 
@@ -53,6 +55,11 @@ public class QuestionController {
 	public Page<Question> getAllQuestions(Pageable pageable)
 	{
 		return questionService.getAllQuestions(pageable);
+	}
+	
+	@PostMapping("/roles")
+	public Collection<GrantedAuthority> getRoles(User u){
+		return userClient.getRoles(u);
 	}
 
 	/**

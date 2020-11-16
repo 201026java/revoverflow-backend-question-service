65eacd025f611e7355b61de974566c0469a515e3
Mark Alsip <mark.alsip@comcast.net>
2020-11-16 09:21:20 -0700

    how is this even a thing?

--- src/main/java/com/revature/clients/UserClient.java
+++ src/main/java/com/revature/clients/UserClient.java
@@ -1,6 +1,10 @@
 package com.revature.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import java.util.Collection;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.revature.models.User;
---
+++ src/main/java/com/revature/models/Roles.java
@@ -0,0 +1,10 @@
+package com.revature.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor 
+public class Roles {
+
+	private String authority;
+}
